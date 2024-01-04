3ebecd249597e46473b76c1bd36349807ddc9733
SimaKarami <simakarami1996@gmail.com>
2024-01-04 14:22:44 +0330

    Add pop up for reset password

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@ element.addEventListener('change', (e) => {
     var card = document.querySelector('.card')
     e.target.checked ? card.classList.add('is-flipped') : card.classList.remove('is-flipped')
 })
+
+//open modal
+var modal = document.getElementById('modal');
+let email = document.getElementById('email');
+let text = document.getElementById('text');
+function OpenModal(){
+    modal.style.display = 'block';
+}
+function CloseModal(){
+    modal.style.display = 'none';
+    email.style.borderColor = '#424874'
+    text.innerHTML = " ";
+    email.value = " ";
+}
+function SendResetEmail(){
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (email.value.match(pattern)) {
+        email.style.borderColor = '#424874'
+        text.innerHTML = " "
+    } else {
+        text.innerHTML = "Please Enter Valid Email Address"
+        email.style.borderColor = 'red'
+    }
+}
