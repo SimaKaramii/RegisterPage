6da7a773d8a75ee6e8f046b325e279f07432c86f
SimaKarami <simakarami1996@gmail.com>
2024-01-05 15:21:29 +0330

    Add sign up form

--- js/script.js
+++ js/script.js
@@ -18,13 +18,45 @@ function CloseModal(){
     text.innerHTML = " ";
     email.value = " ";
 }
-function SendResetEmail(){
+function CheckValidEmail(email , text){
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    var flag = true;
     if (email.value.match(pattern)) {
-        email.style.borderColor = '#424874'
         text.innerHTML = " "
+        flag = true
     } else {
         text.innerHTML = "Please Enter Valid Email Address"
-        email.style.borderColor = 'red'
+        flag = false
+    }
+    return flag
+}
+function SendResetEmail(){
+    CheckValidEmail(email , text)
+}
+var user = document.getElementById('user');
+let emailadderss = document.getElementById('emailadderss');
+let Password = document.getElementById('Password');
+let rePassword = document.getElementById('rePassword');
+let TextSignUp = document.getElementById('TextSignUp');
+var checkboxTerms = document.getElementById("checkboxTerms");
+function SendSignUp(){
+    var flag= true;
+    if(!user.value||!emailadderss.value||!Password.value||!rePassword.value){
+        TextSignUp.innerHTML = "Please fill in all information"
+        flag = false;
+    }
+    else{
+
+        if(emailadderss.value){
+             CheckValidEmail(emailadderss , TextSignUp)
+            flag = CheckValidEmail(emailadderss , TextSignUp);
+        }
+        if(CheckValidEmail(emailadderss , TextSignUp) && Password.value !== rePassword.value){
+            TextSignUp.innerHTML = "Password and repeat password are not the same"
+            flag = false;
+        }
+        if (flag && !checkboxTerms.checked) {
+            TextSignUp.innerHTML = "Please check box if you want to proceed"
+        }
     }
 }
